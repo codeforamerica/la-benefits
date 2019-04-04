0aa3cf99bdafb42d548270e92426add2da10b933
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-04-04 16:18:49 -0400

    Create redirects to intake forms.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   mount Cfa::Styleguide::Engine => "/cfa"
   root to: "pages#main"
+
+  get "/craigslist-intake", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSelkXCnXbpyEb8zsQAqGaR504JKvaABCxWLsrBHPa4wyPmMww/viewform")
+  get "/wic-intake", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSdIHJNE3vGhuZbb0M8KvWBkfDrtlpVJJwGjCcdAwc4WM8kfVg/viewform?entry.614817457=English")
 end
