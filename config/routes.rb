44e604881d9393d343bb02575e0f718b1408f461
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-04-22 16:29:28 -0700

    Add /snap-intake redirect

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "/craigslist-intake", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSelkXCnXbpyEb8zsQAqGaR504JKvaABCxWLsrBHPa4wyPmMww/viewform")
   get "/wic-intake", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSdIHJNE3vGhuZbb0M8KvWBkfDrtlpVJJwGjCcdAwc4WM8kfVg/viewform?entry.614817457=English")
   get "/consent", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSdMpV1DMnuybxsj305W7fFCry9leJP7VNAYggSL4waXVUaHuA/viewform")
+  get "/snap-intake", to: redirect("https://docs.google.com/forms/d/e/1FAIpQLSd1OYkvz-7eU7GbzhqM_bSbV8q1XjrhpsHMKHKoJL_gUTbEjg/viewform?entry.614817457=English")
 end
